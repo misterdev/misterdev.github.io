3a34deee4f9d26ba2419866a1cee0ef7f69c432a
Devid Farinelli <devid.farinelli@gmail.com>
2015-11-28 00:40:59 +0100

    Agiunto tuto

---
+++ src/app.def.js
@@ -0,0 +1,23 @@
+
+          angular
+              .module('starterApp', ['ngMaterial', 'users', 'toolbarDemo1'])
+              .config(function($mdThemingProvider, $mdIconProvider){
+
+                  $mdIconProvider
+                      .defaultIconSet("./assets/svg/avatars.svg", 128)
+                      .icon("menu"       , "./assets/svg/menu.svg"        , 24)
+                      .icon("share"      , "./assets/svg/share.svg"       , 24)
+                      .icon("google_plus", "./assets/svg/google_plus.svg" , 512)
+                      .icon("hangouts"   , "./assets/svg/hangouts.svg"    , 512)
+                      .icon("twitter"    , "./assets/svg/twitter.svg"     , 512)
+                      .icon("phone"      , "./assets/svg/phone.svg"       , 512);
+
+                      $mdThemingProvider.theme('default')
+                          .primaryPalette('brown')
+                          .accentPalette('red');
+
+              });
+
+angular.module('toolbarDemo1', ['ngMaterial'])
+  .controller('AppCtrl', function($scope) {
+  });
