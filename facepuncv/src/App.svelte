75f6e787291f699ecec6007c535b639d167afe64
Devid Farinelli <devid.farinelli@gmail.com>
2019-10-05 17:48:34 +0200

    video bg

---
+++ facepuncv/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let name;
+	let src = 'https://sveltejs.github.io/assets/caminandes-llamigos.mp4'
+</script>
+
+<style>
+	.wrapper {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		align-items: center;
+	}
+	video {
+		width: 100%;
+	}
+</style>
+
+<div class="wrapper">
+	<video {src} autoplay/>
+</div>
