a2d81bce2dafe0ea5ad5966de4506238b8d612b0
Devid Farinelli <devid.farinelli@gmail.com>
2019-10-05 19:34:54 +0200

    crafting layout

--- facepuncv/src/App.svelte
+++ facepuncv/src/App.svelte
@@ -1,20 +1,47 @@
 <script>
+	import Inventory from './Inventory.svelte'
+	import Crafting from './Crafting.svelte'
 	export let name;
-	let src = 'https://sveltejs.github.io/assets/caminandes-llamigos.mp4'
+	// let src = 'https://sveltejs.github.io/assets/caminandes-llamigos.mp4'
+	let src = 'https://video.xx.fbcdn.net/v/t42.9040-2/68135311_2342255342534112_4239331015497089024_n.mp4?_nc_cat=108&efg=eyJ2ZW5jb2RlX3RhZyI6InN2ZV9zZCJ9&_nc_oc=AQnD8UbJiLvZ7DpjjSWnxGYR_HSynb8VXzKOwyjkyvpEW_NiLXnDJqGTFA5FGj6Go4E&_nc_ht=video-mxp1-1.xx&oh=70776a3ab340b170f6c9e49d80f9fce7&oe=5D98ECE8'
+	let selected = Crafting
 </script>
+
+<div class="wrapper">
+	<div class="overlay"></div>
+	<video {src} autoplay loop muted/>
+	<div id="content">
+		<svelte:component this={selected} />
+	</div>
+</div>
 
 <style>
 	.wrapper {
-		width: 100%;
-		height: 100%;
+		width: 100vw;
+		height: 100vh;
 		display: flex;
 		align-items: center;
 	}
+	.overlay {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: white;
+		opacity: 0;
+	}
 	video {
 		width: 100%;
+		filter: blur(8px);
+		-webkit-filter: blur(8px);
+	}
+
+	#content {
+		position: fixed;
+		width: 100vw;
+		height: calc(100vw / 16 * 9);
+		/* background-color: blue;
+		opacity: .8; */
 	}
 </style>
-
-<div class="wrapper">
-	<video {src} autoplay/>
-</div>
---
+++ facepuncv/src/Crafting.svelte
@@ -0,0 +1,67 @@
+<script>
+</script>
+
+<div id="wrapper">
+    <div id="content">
+        <div id="categories"></div>
+        <div id="list"></div>
+        <div id="queue"></div>
+        <div id="description"></div>
+        <div id="skins"></div>
+        <div id="stats"></div>
+    </div>
+</div>
+
+<style>
+    #categories {
+        grid-area: cats;
+        background-color: red;
+    }
+    #list {
+        grid-area: list;
+        background-color: blue;
+    }
+    #queue {
+        grid-area: queu;
+        background-color: green;
+    }
+    #description {
+        grid-area: desc;
+        background-color: yellow;
+    }
+    #skins {
+        grid-area: skin;
+        background-color: magenta;
+    }
+    #stats {
+        grid-area: stat;
+        background-color: orange;
+    }
+    #wrapper {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        padding: 40px;
+        overflow: hidden;
+        background: greenyellow;
+        opacity: .7;
+        box-sizing: border-box;
+    }
+    #content {
+        width: 100%;
+        height: 100%;
+        display: grid;
+        grid-template-areas: 
+            "cats list list list desc desc desc desc"
+            "cats list list list desc desc desc desc"
+            "cats list list list desc desc desc desc"
+            "cats list list list skin skin skin skin"
+            "cats list list list skin skin skin skin"
+            "cats list list list skin skin skin skin"
+            "cats list list list stat stat stat stat"
+            "cats list list list stat stat stat stat"
+            "queu queu queu queu stat stat stat stat";
+        grid-column-gap: 2px;
+        grid-row-gap: 2px;
+    }
+</style>
